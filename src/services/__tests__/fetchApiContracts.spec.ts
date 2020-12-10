238709c14fef31788dd3f19e209ed9c2661cc905
Jimmy Jardland <jimmy@jardland.se>
2020-12-10 13:12:30 +0100

    test: for fetchapi

---
+++ src/services/__tests__/fetchApiContracts.spec.ts
@@ -0,0 +1,66 @@
+import { client } from '../../adapters/api'
+import { Contract } from '../../types'
+import {
+  getSelectionById,
+  updateContract,
+  addContractToSelection,
+} from '../../services/db'
+
+import { fetchApiContracts } from '../fetchApiContracts'
+
+jest.mock('../../adapters/api')
+
+jest.mock('../../services/db')
+
+describe('fetchApiContracts', () => {
+  test('calls getSelectionById', async () => {
+    ;(client.get as jest.Mock).mockResolvedValueOnce([{ id: 1 }])
+    ;(updateContract as jest.Mock).mockResolvedValueOnce('a9231923')
+    ;(getSelectionById as jest.Mock).mockResolvedValueOnce({
+      selection_term: 2,
+    })
+
+    await fetchApiContracts('1337')
+
+    expect(getSelectionById).toBeCalledWith('1337')
+  })
+
+  test('gets selection', async () => {
+    ;(client.get as jest.Mock).mockResolvedValueOnce([{ id: 1 }])
+    ;(updateContract as jest.Mock).mockResolvedValueOnce('a9231923')
+    ;(getSelectionById as jest.Mock).mockResolvedValueOnce({
+      selection_term: 2,
+    })
+
+    await fetchApiContracts('1337')
+
+    expect(client.get).toBeCalledWith({
+      url:
+        'leasecontracts/?rentalid=2*&includetenants=true&includerentals=true',
+    })
+  })
+
+  test('calls updateContract', async () => {
+    ;(client.get as jest.Mock).mockResolvedValueOnce([{ id: 1 }])
+    ;(updateContract as jest.Mock).mockResolvedValueOnce('a9231923')
+    ;(getSelectionById as jest.Mock).mockResolvedValueOnce([
+      { selection_term: 2 },
+    ])
+
+    await fetchApiContracts('1337')
+
+    expect(updateContract).toBeCalledWith({ id: 1 })
+  })
+
+  test('adds contract to selection', async () => {
+    ;(client.get as jest.Mock).mockResolvedValueOnce([{ id: 1 }])
+    ;(updateContract as jest.Mock).mockResolvedValueOnce('a9231923')
+    ;(getSelectionById as jest.Mock).mockResolvedValueOnce([
+      { selection_term: 2 },
+    ])
+
+    await fetchApiContracts('1337')
+
+    expect(addContractToSelection).toBeCalledWith('a9231923', '1337')
+  })
+})
