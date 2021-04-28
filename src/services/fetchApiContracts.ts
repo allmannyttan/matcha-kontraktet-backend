db2ed0deaba25e49b035b72f5c93568f2054bcd5
Radu Achim <radu.achim@iteam.se>
2021-04-28 08:55:35 +0200

    refactor: 💡 revert changes made for debugging

--- src/services/fetchApiContracts.ts
+++ src/services/fetchApiContracts.ts
@@ -9,14 +9,12 @@ import {
 export const fetchApiContracts = async (selectionId: string) => {
   const { selection_term, from, to } = await getSelectionById(selectionId)
 
-  const url = `leasecontracts/?includetenants=true&includerentals=true${
-    selection_term ? `&rentalid=${selection_term}*` : ''
-  }${from ? `&from=${new Date(from).toISOString()}` : ''}${
-    to ? `&to=${new Date(to).toISOString()}` : ''
-  }`
-
   const contracts = await client.get({
-    url: url,
+    url: `leasecontracts/?includetenants=true&includerentals=true${
+      selection_term ? `&rentalid=${selection_term}*` : ''
+    }${from ? `&from=${new Date(from).toISOString()}` : ''}${
+      to ? `&to=${new Date(to).toISOString()}` : ''
+    }`,
   })
 
   for (const contract of contracts) {
